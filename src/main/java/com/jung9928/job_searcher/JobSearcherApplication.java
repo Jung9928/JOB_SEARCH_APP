6f773072ce9d6c5786866fc296c106f6100771a2
Jung9928 <fg43irks@naver.com>
2023-12-25 19:22:13 +0900

    feat : 프로젝트 초기 설정 셋팅

---
+++ src/main/java/com/jung9928/job_searcher/JobSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.jung9928.job_searcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jung9928/job_searcher/JobSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jung9928.job_searcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
